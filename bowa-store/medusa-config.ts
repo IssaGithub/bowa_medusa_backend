489a8823b153bea867839b7f0c6a708a60f859ff
issa <issa.zaytoun@googlemail.com>
2025-06-14 10:21:06 +0200

    try to handle cors

--- bowa-store/medusa-config.ts
+++ bowa-store/medusa-config.ts
@@ -4,13 +4,12 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
-  }
+      storeCors: process.env.STORE_CORS || "http://localhost:8000,http://localhost:4321",
+      adminCors: process.env.ADMIN_CORS || "http://localhost:9000,http://localhost:4321",
+      authCors: process.env.AUTH_CORS || "http://localhost:8000,http://localhost:9000,http://localhost:4321",
+    },
+    // ...
+  },
 })
+
